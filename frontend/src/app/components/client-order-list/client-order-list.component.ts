600a956233f559c33629e5da3d6f6df948deb548
BrayanPCH <bapulamarinc@utn.edu.ec>
2022-07-08 16:28:19 -0500

    Corrección de errores.

--- frontend/src/app/components/client-order-list/client-order-list.component.ts
+++ frontend/src/app/components/client-order-list/client-order-list.component.ts
@@ -3,6 +3,10 @@ import { ModelPedido } from '../../model/model.pedido';
 import { PedidosService } from '../../service/pedidos.service';
 import { ModelPedidoDetalle } from '../../model/model.pedido-detalle';
 
+
+import { jsPDF } from 'jspdf';
+import 'jspdf-autotable';
+
 import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
@@ -101,46 +105,93 @@ export class ClientOrderListComponent implements OnInit {
     )
   }
 
+  header = [['Código', 'Nombre Producto', 'Precio', 'Cantidad', 'Total']]
+
+  tableData: any = []
+
+  public llenarTableData() {
+    var dataTable: any = []
+    this.detallesPedido.forEach(detalle => {
+      dataTable.push(detalle.codigo_prod);
+      dataTable.push(detalle.pro_nombre);
+      dataTable.push(detalle.ped_det_unitario);
+      dataTable.push(detalle.ped_det_cant);
+      dataTable.push(detalle.ped_det_total);
+      this.tableData.push(dataTable);
+    });
+  }
+
   public generarComprobantePedido() {
-    var num = 2.111;
-    var num2 = num.toFixed(2);
-    // var generateData = function (amount: any) {
-    //   var result = [];
-    //   var data = {
-    //     coin: "100",
-    //     game_group: "GameGroup",
-    //     game_name: "XPTO2",
-    //     game_version: "25",
-    //     machine: "20485861",
-    //     vlt: "0"
-    //   };
-    //   for (var i = 0; i < amount; i += 1) {
-    //     data.coin = (i + 1).toString();
-    //     result.push(Object.assign({}, data));
-    //   }
-    //   return result;
-    // };
+    this.llenarTableData();
+
+    console.log(this.detallesPedido);
+    console.log(this.tableData);
+
 
     // const doc = new jsPDF('p', 'pt', 'a4');
-    // var result = [];
-    // result.push({
-    //   id: "Código Producto",
-    //   name: "Nombre Producto",
-    //   price: "Precio Producto",
-    //   cant: "Cantidad",
-    //   total: "Total"
-    // });
+    const doc = new jsPDF();
 
-    // var headers = {
-    //   id: "Código Producto",
-    //   name: "Nombre Producto",
-    //   price: "Precio Producto",
-    //   cant: "Cantidad",
-    //   total: "Total"
-    // };
-    // doc.text("Reporte", 105, 80);
-    // // doc.table(1, 1, generateData(100), headers, { autoSize: true });
-    // doc.save("prueba.pdf");
+    // -------------------------------------------------------------------------------------------
+    //                  HEADER ORDER PDF
+    // -------------------------------------------------------------------------------------------
+
+    doc.setFontSize(26);
+    doc.text("Shopping Cart", 105, 35, { align: 'center' });
+    doc.text("This is centred text.", 105, 80, { align: 'center' });
+    doc.text("Datos del Pedido", 105, 90, { align: 'center' });
+    doc.text("Código Pedido: ", 105, 100);
+    doc.text("PC000001", 105, 100);
+    doc.text("Fecha de Pedido:", 105, 110);
+    doc.text("08/07/2022:", 105, 110);
+    doc.text("Datos del Cliente", 105, 120);
+    doc.text("Cédula:", 105, 130);
+    doc.text("Nombres:", 105, 130);
+    doc.text("Brayan Pulamarin:", 105, 80);
+    doc.text("Correo:", 105, 80);
+    doc.text("bapulamarinc@utn.edu.ec:", 105, 80);
+    doc.text("Teléfono:", 105, 80);
+    doc.text("0999999999", 105, 80);
+
+    // -------------------------------------------------------------------------------------------
+    //                  DETAILS ORDER PDF
+    // -------------------------------------------------------------------------------------------
+
+    doc.setFontSize(18);
+    doc.text('My Team Detail', 11, 8);
+    doc.setFontSize(11);
+    doc.setTextColor(100);
+
+    (doc as any).autoTable({
+      head: this.header,
+      body: this.tableData,
+      theme: 'plain',
+      margin:{top: 100}
+      // didDrawCell: data => {
+      //   console.log(data.column.index)
+      // }
+    })
+
+    // -------------------------------------------------------------------------------------------
+    //                  FOOTER ORDER PDF
+    // -------------------------------------------------------------------------------------------
+
+    doc.text("Subtotal:", 105, 200);
+    doc.text("100", 105, 80);
+    doc.text("Iva (12%):", 105, 80);
+    doc.text("1.20", 105, 80);
+    doc.text("Total:", 105, 80);
+    doc.text("11.20", 105, 80);
+
+    doc.text("____________________________________________", 105, 80);
+    doc.text("1727468512", 105, 80);
+    doc.text("Brayan Pulamarin", 105, 80);
+
+    doc.text("________________________________________________________________________________________________________________", 105, 80);
+    doc.text("Este documento cuenta con una validez de 30 días hábiles.", 105, 80);
+    doc.text("Fecha Generación Documento: 08/07/2022", 105, 80);
+    doc.text("________________________________________________________________________________________________________________", 105, 80);
+
+    doc.output('dataurlnewwindow');
   }
 
 }
