63508bbc6ee020349f16722fa163cd9b46e04418
jacarcelenb <carcelenjorge17@gmail.com>
2022-07-17 16:30:46 -0500

    Cambios en el frontend

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig :{
+  apiKey: "AIzaSyC8vo3rfYb8TxQfl9OQQVRXDlaI6M-vxZA",
+  authDomain: "app-shopping-cart-52d46.firebaseapp.com",
+  projectId: "app-shopping-cart-52d46",
+  storageBucket: "app-shopping-cart-52d46.appspot.com",
+  messagingSenderId: "471996855625",
+  appId: "1:471996855625:web:3cfb69fc5b4fe8ecea5698",
+  measurementId: "G-WJDVGCB2HN"
+}
 };
